1a22bdd0e700756bd27db80c984fc6d1cacf6677
687edf53 <sm3257698@gmail.com>
2023-11-02 17:55:28 +0200

    new Update

--- js/main.js
+++ js/main.js
@@ -2,15 +2,40 @@
 let cartIcon = document.querySelector('#cart-icon')
 let cart = document.querySelector('.cart')
 let closeCart = document.querySelector('#close-cart')
-
+let shopContainer = document.querySelector('.shop')
+let shopContent = document.querySelector('.shop-content')
+let shopTitle = document.querySelector('.section-title')
 // Open Cart
+
+let input_global = 1;
+
+let distance = (window.innerWidth - (shopContainer.offsetWidth + cart.offsetWidth))
 cartIcon.addEventListener('click', () => {
   cart.classList.add('active')
+  updateTotal()
+  shopContainer.style.margin = '3rem 0'
+  shopContent.style.padding = '0 20px'
+  shopContainer.style.alignItems = 'center'
+  shopContainer.style.width = (window.innerWidth - cart.offsetWidth) + 'px'
+  shopContent.style.width = (window.innerWidth - cart.offsetWidth) + 'px'
+  showData()
 })
+
 
 // Close Cart
 closeCart.addEventListener('click', () => {
   cart.classList.remove('active')
+  shopContent.style.width = '100%'
+  shopContainer.style.margin = '3rem'
+  if (document.body.clientWidth > 900) {
+    shopContainer.style.alignItems = 'center'
+    shopContainer.style.width = '100%'
+  }
+  else {
+    shopContainer.style.alignItems = 'none'
+    shopContainer.style.padding = '0'
+    shopContainer.style.width = 'fit-content'
+  }
 })
 
 // Cart Working JS
@@ -44,14 +69,30 @@ function removeCartItem(e) {
   let parentButton = e.target.parentElement
   parentButton.remove()
   updateTotal()
+  for (let i = 0; i < dataPro.length; i++) {
+    let title = parentButton.children[1].children[0].innerHTML
+    dataPro.splice(dataPro.indexOf(title), 1)
+    localStorage.products = JSON.stringify(dataPro)
+  }
+  updateTotal()
 }
 
 // Quantity Changes
 function quantityChanged(e) {
   let input = e.target;
+  let title = e.target.parentElement.children[0].innerHTML
+
   if (isNaN(input.value) || input.value <= 0) {
     input.value = 1
   }
+  for (let i = 0; i < dataPro.length; i++) {
+    if (dataPro[i].title === title) {
+      dataPro[i].input = input.value
+      localStorage.products = JSON.stringify(dataPro)
+    }
+  }
+
+
   updateTotal()
 }
 
@@ -66,30 +107,89 @@ function addCartItem(e) {
   updateTotal()
 }
 
+let dataPro;
+if (localStorage.getItem('products')) {
+  dataPro = JSON.parse(localStorage.products)
+} else {
+  dataPro = []
+}
+
+function showData() {
+  let cartContent = document.querySelector('.cart-content')
+  let cartBox
+  let allBoxes = ""
+  for (let i = 0; i < dataPro.length; i++) {
+
+    cartBox = `
+     <div class="cart-box">
+     <img src="${dataPro[i].img}" class="cart-img">
+     <div class="detail-box">
+     <div class="cart-product-title">${dataPro[i].title}</div>
+     <div class="cart-price">${dataPro[i].price}</div>
+     <input type="number" value="${dataPro[i].input}" min="1" class="cart-quantity">
+     </div>
+     <!-- Remove Cart -->
+     <i class="bx bxs-trash-alt cart-remove"></i>
+     </div>`
+
+    allBoxes += cartBox
+  }
+
+
+  cartContent.innerHTML = allBoxes
+
+  for (let i = 0; i < cartContent.children.length; i++) {
+    cartContent.children[i].getElementsByClassName('cart-remove')[0].addEventListener('click', removeCartItem)
+    cartContent.getElementsByClassName('cart-quantity')[0].addEventListener('input', quantityChanged)
+  }
+}
+showData()
 
 function addProductToCart(title, price, img) {
   let cartShopBox = document.createElement('div')
-  cartShopBox.classList.add('cart-box')
-  let cartItems = document.getElementsByClassName('cart-content')[0]
-  let cartItemsNames = cartItems.getElementsByClassName('cart-product-title')
 
-  for (let i = 0; i < cartItemsNames.length; i++) {
-    alert('You Have already add this item to cart')
+  let have = false;
+  for (let i = 0; i < dataPro.length; i++) {
+    if (dataPro[i].title == title) {
+      have = true
+    }
+    else {
+      have = false
+    }
   }
-  let cartBoxContent = `
-    <img src="${img}" class="cart-img">
+  
+  if(have) {
+    alert('you have it')
+  }
+  else {
+    cartShopBox.classList.add('cart-box')
+    let cartItems = document.getElementsByClassName('cart-content')[0]
+    let input = 1;
+    let obj = {
+      title: title,
+      price: price,
+      img: img,
+      input: input
+    }
+    dataPro.push(obj)
+    localStorage.setItem('products', JSON.stringify(dataPro))
+
+    cartBoxContent = `
+  <img src="${img}" class="cart-img">
     <div class="detail-box">
       <div class="cart-product-title">${title}</div>
       <div class="cart-price">${price}</div>
       <input type="number" value="1" min="1" class="cart-quantity">
-    </div>
-    <!-- Remove Cart -->
-    <i class="bx bxs-trash-alt cart-remove"></i>`
+      </div>
+      <!-- Remove Cart -->
+      <i class="bx bxs-trash-alt cart-remove"></i>`
 
-  cartShopBox.innerHTML = cartBoxContent;
-  cartItems.append(cartShopBox);
-  cartShopBox.getElementsByClassName('cart-remove')[0].addEventListener('click', removeCartItem)
-  cartShopBox.getElementsByClassName('cart-quantity')[0].addEventListener('input', quantityChanged)
+    cartShopBox.innerHTML = cartBoxContent;
+    cartItems.append(cartShopBox);
+    cartShopBox.getElementsByClassName('cart-remove')[0].addEventListener('click', removeCartItem)
+    cartShopBox.getElementsByClassName('cart-quantity')[0].addEventListener('input', quantityChanged)
+  }
+  
 }
 
 // Update Total 
@@ -111,3 +211,9 @@ function updateTotal() {
   }
 
 }
+
+window.addEventListener('keydown', (e) => {
+  if (e.keyCode === 67) {
+    cart.classList.toggle('active')
+  }
+})
